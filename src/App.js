fabc1b2b268768b026d5eac25b39ad95b2ed7ddb
Ahmadreza <ahmadrezaghafari62@email.com>
2026-04-16 09:27:15 +0100

    fix certificate route order

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Admin from "./Admin";
 import Payment from "./Payment";
 import Training from "./Training";
 import Contact from "./Contact";
+import Certificate from "./Certificate";
 import Completed from "./Completed";
 
 /* ROUTE GUARDS */
